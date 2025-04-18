5626ba0f11323da48effc53adf92f1deccb4b5ba
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-18 10:52:47 +0530

    [Deepak]: user slf4j for logging message, create lockback.xml for log formating

--- src/main/java/com/bridgelab/logger/LoggerApplication.java
+++ src/main/java/com/bridgelab/logger/LoggerApplication.java
@@ -1,21 +1,29 @@
 package com.bridgelab.logger;
 
+
 import com.bridgelab.logger.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+
+
+
 @SpringBootApplication
 public class LoggerApplication {
+    public static final Logger logger = (Logger) LoggerFactory.getLogger(LoggerApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        logger.debug("\nWelcome to Spring Concept Demo");
 
-		System.out.println("Hello spring boot");
+        ApplicationContext context = SpringApplication.run(LoggerApplication.class, args);
 
-		ApplicationContext context = SpringApplication.run(LoggerApplication.class, args);
+        logger.debug("\nChecking Context: {}", context.getBean(DemoBean.class));
 
-		System.out.println(context.getBean(DemoBean.class));
+        logger.debug("\n*** Example Using @Autowired annotation on property ***");
+    }
 
-	}
 
 }
